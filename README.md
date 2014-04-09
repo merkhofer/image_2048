c265a3eba3856144e151cadef6afde7c181f33e8
Liz Merkhofer <lmerkhofer@bericotechnologies.com>
2014-04-08 23:32:36 -0400

    working image search

---
+++ README.md
@@ -0,0 +1,3 @@
+# 2048
+This is a clone of [2048](http://gabrielecirulli.github.io/2048/), but will fill tiles with the top image results from Bing. Did you ever want to play Muppets2048 or Liz2048? You're welcome.
+
---
+++ bing.js
@@ -0,0 +1,45 @@
+
+/*! Bing Search Helper v1.0.0 - requires jQuery v1.7.2 */
+$(function()
+{
+  // Attaches a click handler to the button.
+  $('#bt_search').click(function(e)
+  {
+    e.preventDefault();
+    // Clear the results div.
+    $('#results').empty();
+    var query = $('#query').val();
+    var serviceOp = $('input[name=service_op]:checked', '#my_form').val();
+    if (query)
+      thumbs = search(query, serviceOp);
+      var actuated_bing = new BingActuator(thumbs);
+      new GameManager(4, KeyboardInputManager, actuated_bing, LocalStorageManager);
+      $("#game_title").text(query+' 2048');
+      $("#game").show();
+      $("#my_form").hide();
+      $('#new_query').show();
+  });
+
+  function search(query, serviceOp)
+  {
+    // Establish the data to pass to the proxy.
+    var data = { q: query, sop: serviceOp, market: 'en-us' };
+    var hashOfThumbs = {};
+    // Calls the proxy, passing the query, service operation and market.
+    $.getJSON('bing_proxy.php', data, function(obj)
+      {
+        if (obj.d !== undefined)
+        {
+          var items = obj.d.results;
+          for (var k=0; k < 12; k++)
+          {
+            thisTile = (Math.pow(2, (k+1))).toString();
+            hashOfThumbs[thisTile] = items[k].Thumbnail.MediaUrl;
+            }
+          }
+        });
+    //alert(hashOfThumbs);
+    return hashOfThumbs;
+}
+
+});
